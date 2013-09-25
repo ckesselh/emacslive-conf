94dc39385dd78bf83cd06dc095a1d27b38e86b9c
Christian Kesselheim <christian.kesselheim@kreios.lu>
2013-09-25 18:23:34 +0200

    Simplified some LISP-specific keybindings

--- ckesselh-pack/config/bindings.el
+++ ckesselh-pack/config/bindings.el
@@ -7,3 +7,21 @@
 ;; Find file in local GIT repository
 (define-key global-map (kbd "C-x C-g") 'helm-ls-git-ls)
 (define-key global-map (kbd "C-x C-r") 'recentf-open-files)
+
+;; The standard keybindings for resizing windows are just plain crazy
+(define-key global-map (kbd "S-C-<left>") 'shrink-window-horizontally)
+(define-key global-map (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(define-key global-map (kbd "S-C-<up>") 'shrink-window)
+(define-key global-map (kbd "S-C-<down>") 'enlarge-window)
+
+;; Same for sexpr navigation
+(define-key lisp-mode-shared-map (kbd "C-s-<left>") 'paredit-backward)
+(define-key lisp-mode-shared-map (kbd "C-s-<right>") 'paredit-forward)
+(define-key lisp-mode-shared-map (kbd "C-s-<up>") 'paredit-backward-up)
+(define-key lisp-mode-shared-map (kbd "C-s-<down>") 'paredit-forward-up)
+
+;; Some custom key combo to support copying whole sexpr at once
+(defun copy-sexp-at-point ()
+  (interactive)
+  (kill-new (thing-at-point 'sexp)))
+(define-key lisp-mode-shared-map [C-s-268632075] 'copy-sexp-at-point)
