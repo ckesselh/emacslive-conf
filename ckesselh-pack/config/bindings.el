6cecf48197cd901470cc59bd7bdf2b985b991a95
Christian Kesselheim <christian.kesselheim@kreios.lu>
2013-11-10 11:40:03 +0100

    Added keybinding to transpose windows

--- ckesselh-pack/config/bindings.el
+++ ckesselh-pack/config/bindings.el
@@ -38,5 +38,19 @@
   (kill-new (thing-at-point 'sexp)))
 (define-key lisp-mode-shared-map [C-s-268632075] 'copy-sexp-at-point)
 
+;; Some custom key combo to swap the content of the 2 currently adjoining windows
+(defun transpose-windows (arg)
+  "Transpose the buffers shown in two windows."
+  (interactive "p")
+  (let ((selector (if (>= arg 0) 'next-window 'previous-window)))
+    (while (/= arg 0)
+      (let ((this-win (window-buffer))
+            (next-win (window-buffer (funcall selector))))
+        (set-window-buffer (selected-window) next-win)
+        (set-window-buffer (funcall selector) this-win)
+        (select-window (funcall selector)))
+      (setq arg (if (plusp arg) (1- arg) (1+ arg))))))
+(define-key global-map (kbd "S-C-t") 'transpose-windows)
+
 ;; Unbind some of the more confusing defaults that I typically never use
 (global-unset-key (kbd "C-z"))
